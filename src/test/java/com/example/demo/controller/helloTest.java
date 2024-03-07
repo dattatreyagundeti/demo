6f0f0f7592d1c6afcfca1c33642062324b800edb
dattatreya gundeti <dattatreyagundeti@gmail.com>
2024-03-07 15:30:16 +0530

    Added hello world Api

---
+++ src/test/java/com/example/demo/controller/helloTest.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.controller.hello;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(hello.class) 
+public class helloTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("hello world"));
+    }
+}
